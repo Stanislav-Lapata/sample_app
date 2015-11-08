20e03a4bd87c39326dd96896a29fe6d72fedacaa
Stanislav <lers@list.ru>
2015-11-08 17:02:34 +0300

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do	
+	factory :user do
+		name									"LersSett"
+		email 								"LersSet@gmail.com"
+		password 							"Nail30031812"
+		password_confirmation "Nail30031812"
+	end
+end
